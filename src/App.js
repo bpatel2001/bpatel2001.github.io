b39ab8c9f5c0210a3d15ec3df3ef0f53aae9145b
bpatel2001 <125521942+bpatel2001@users.noreply.github.com>
2024-11-16 14:13:26 -0600

    react + tailwind

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className = " text-green-50 font-bold text-center text-2xl">
+        Hi! My name is Bhavik Patel
+      </header>
+      <p className="text-blue-50 font-bold">
+        Bruh
+      </p>
+    </div>
+  );
+}
+
+export default App;
